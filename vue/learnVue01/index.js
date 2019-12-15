d915497377fba0f1161859ec525f21a269c16748
zuogerenba <1732935973@qq.com>
2019-12-15 20:43:55 +0800

    Vue

--- vue/learnVue01/index.js
+++ vue/learnVue01/index.js
@@ -1,8 +1,31 @@
-const Vue = require('vue');
-const app = Vue ({
+const app = new Vue ({
     el: '#app', //挂载要管理的元素
     data:{      //定义数据
         message:"你好啊，吃了吗？"
     }
-
 })
+const movie = new Vue ({
+    el:'#movie',
+    data:{
+        message:'你好啊',
+        movies:['海王','星际穿越','大话西游','盗梦空间']
+    }
+})
+const counter = new Vue ({
+    el:'#counter',
+    data:{
+        message:'当前计数:',
+        counter:0
+    },
+    methods:{
+        add: function(){
+            console.log("add被执行");
+            this.counter++
+        },
+        sub: function(){
+            console.log("sub被执行");
+            this.counter--
+        }
+    }
+})
+
---
+++ vue/readme.md
@@ -0,0 +1,9 @@
+el:'#app' 用于挂载要管理的元素 是一个id名
+以前的js(编程范式：命令式)
+vue:声明式
+优点：数据和界面分离 处理数据方便
+<li v-for="item in movies">{{item}}</li> 遍历movies
+函数再methods 中定义 methods:{ functionName:function(){} }
+MVVM: Model View=DOM
+console.log快捷键 clg
+function 快捷键  

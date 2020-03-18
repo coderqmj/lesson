de4d692668010cf41c36c27f022a0d910e2ac088
zuogerenba <1732935973@qq.com>
2020-03-18 21:03:16 +0800

    flex

--- css/flex/readme.md
+++ css/flex/readme.md
@@ -12,3 +12,61 @@
 - 2.**main start** 和 **main end** 分别代表主轴的开始和结束
 - 3.**cross axis** 交叉轴是向下的竖轴
 - 4.**cross start** **cross end** 分别代表 交叉轴的开始和结束
+
+### 三.flex的属性
+#### 1.flex-direction
+
+- 作用：决定主轴的方向
+
+- `flex-direction`默认值为:row从左向右
+- `flex-direction: row-reverse`; 反向排列，从右到左 
+- `flex-direction: column`从上到下
+
+#### 2.justify-conten 
+- **作用**:决定items在main aixs对齐方式
+- **默认值**:`flex-start` 开始的方向对齐
+- `flex-end`在末端对齐
+- `justify-content: center;`可以使其居中对齐
+  - ![1584507731682](C:\Users\邱\AppData\Roaming\Typora\typora-user-images\1584507731682.png)
+
+
+
+- ` justify-content: space-between;` 左右贴紧 中间等分
+  - ![1584508026825](C:\Users\邱\AppData\Roaming\Typora\typora-user-images\1584508026825.png)
+- `justify-content: space-evenly;` 等分 **开发中最常用**
+- ``justify-content: space-around`
+
+#### 3.align-items
+
+- **作用**：决定了`flex items`在水平方向上的对齐方式 
+- normal：在弹性布局中，效果和stretch一样
+- stretch：当flex items 在 cross axis方向上为auto 是时，会自动拉伸填充flex container
+- flex-start：与cross start 对齐
+- flex-end 与cross end 对齐
+- center： 居中对齐
+- baseline：与基准线对齐
+
+#### 4.flex-wrap
+
+- 原因：默认情况下，所有的item都在一行显示，放不下时item的宽度会被压缩
+- `flex-wrap:nowrap`:不换行显示
+- `flex-wrap:wrap `: 可以换行
+- ![1584527157158](C:\Users\邱\AppData\Roaming\Typora\typora-user-images\1584527157158.png)
+
+#### 5.align-content
+
+- **作用**：`align-content `决定了多行`flex items`在`cross axis`**竖的**上的对齐方式,用法与`justify-content`类似
+- **使用**：在多行 **flex**使用
+- `stretch`( 默认值) :与`align-items`的`stretch`类似
+- `flex-start/end/center` :与`cross start/end/居中` 对齐
+  - ![1584528987203](C:\Users\邱\AppData\Roaming\Typora\typora-user-images\1584528987203.png)
+- `space-between `:
+  - `flex items`之间的距离相等
+  - 与`cross start`、 `cross end`两端对齐
+- `space-around `:
+  -  `flex items`之间的距离相等
+  -  `flex items`与`cross start`、`cross end`之间的距离是`flex items`之间距离的一半
+- `space-evenly `:
+  - `flex items`之间的距离相等
+  - `flex items`与`cross start`、`cross end`之间的距离等于`flex items`之间的距离
+
--- vue/vueProject/color/src/views/colors/Color.vue
+++ vue/vueProject/color/src/views/colors/Color.vue
@@ -2,20 +2,68 @@
   <div class="color">
     <audio src="../../assets/sound/成功.wav" id="audio"></audio>
     <div class="line1">
-      <div class="box orange" @click="copy(0)">
-        <div class="copy">COPY</div>
+      <div class="box a" @click="copy(0)" @mouseenter="a(0)" @mouseleave="b()">
+        <div class="copy" v-show="currentIndex===0">COPY</div>
       </div>
-      <div class="box black" @click="copy(1)">
-        <div class="copy">COPY</div>
+      <div class="box b" @click="copy(1)" @mouseenter="a(1)" @mouseleave="b()">
+        <div class="copy" v-show="currentIndex===1">COPY</div>
       </div>
-      <div class="box black" @click="copy(2)">
-        <div class="copy">COPY</div>
+      <div class="box c" @click="copy(2)" @mouseenter="a(2)" @mouseleave="b()">
+        <div class="copy" v-show="currentIndex===2">COPY</div>
       </div>
-      <div class="box black" @click="copy(3)">
-        <div class="copy">COPY</div>
+      <div class="box d" @click="copy(3)" @mouseenter="a(3)" @mouseleave="b()">
+        <div class="copy" v-show="currentIndex===3">COPY</div>
       </div>
-      <div class="box black" @click="copy(4)">
-        <div class="copy">COPY</div>
+      <div class="box e" @click="copy(4)" @mouseenter="a(4)" @mouseleave="b()">
+        <div class="copy" v-show="currentIndex===4">COPY</div>
+      </div>
+      <!-- --------------------------五个分界线----------------------------------- -->
+      <div class="box f" @click="copy(5)" @mouseenter="a(5)" @mouseleave="b()">
+        <div class="copy" v-show="currentIndex===5">COPY</div>
+      </div>
+      <div class="box g" @click="copy(6)" @mouseenter="a(6)" @mouseleave="b()">
+        <div class="copy" v-show="currentIndex===6">COPY</div>
+      </div>
+      <div class="box h" @click="copy(7)" @mouseenter="a(7)" @mouseleave="b()">
+        <div class="copy" v-show="currentIndex===7">COPY</div>
+      </div>
+      <div class="box i" @click="copy(8)" @mouseenter="a(8)" @mouseleave="b()">
+        <div class="copy" v-show="currentIndex===8">COPY</div>
+      </div>
+      <div class="box j" @click="copy(9)" @mouseenter="a(9)" @mouseleave="b()">
+        <div class="copy" v-show="currentIndex===9">COPY</div>
+      </div>
+      <!-- --------------------------十个分界线--------------------------------------- -->
+      <div class="box k" @click="copy(10)" @mouseenter="a(10)" @mouseleave="b()">
+        <div class="copy" v-show="currentIndex===10">COPY</div>
+      </div>
+      <div class="box l" @click="copy(11)" @mouseenter="a(11)" @mouseleave="b()">
+        <div class="copy" v-show="currentIndex===11">COPY</div>
+      </div>
+      <div class="box m" @click="copy(12)" @mouseenter="a(12)" @mouseleave="b()">
+        <div class="copy" v-show="currentIndex===12">COPY</div>
+      </div>
+      <div class="box n" @click="copy(13)" @mouseenter="a(13)" @mouseleave="b()">
+        <div class="copy" v-show="currentIndex===13">COPY</div>
+      </div>
+      <div class="box o" @click="copy(14)" @mouseenter="a(14)" @mouseleave="b()">
+        <div class="copy" v-show="currentIndex===14">COPY</div>
+      </div>
+      <!-- --------------------------十五个分界线----------------------------------- -->
+      <div class="box p" @click="copy(15)" @mouseenter="a(15)" @mouseleave="b()">
+        <div class="copy" v-show="currentIndex===15">COPY</div>
+      </div>
+      <div class="box q" @click="copy(16)" @mouseenter="a(16)" @mouseleave="b()">
+        <div class="copy" v-show="currentIndex===16">COPY</div>
+      </div>
+      <div class="box r" @click="copy(17)" @mouseenter="a(17)" @mouseleave="b()">
+        <div class="copy" v-show="currentIndex===17">COPY</div>
+      </div>
+      <div class="box s" @click="copy(18)" @mouseenter="a(18)" @mouseleave="b()">
+        <div class="copy" v-show="currentIndex===18">COPY</div>
+      </div>
+      <div class="box t" @click="copy(19)" @mouseenter="a(19)" @mouseleave="b()">
+        <div class="copy" v-show="currentIndex===19">COPY</div>
       </div>
     </div>
   </div>
@@ -26,12 +74,23 @@ export default {
   name: "Color",
   data() {
     return {
-      currentColor: ["#55efc3", "#000"],
+      currentColor: [
+        "#55efc3","#81ecec","#74b9ff","#a29bfe","#dfe6e9",
+        "#00b894","#00cec9","#0984e3","#6c5ce7","#b2bec3",
+        "#ffeaa7","#fab1a0","#ff7675","#fd79a8","#636e72",
+        "#fdcb6e","#e17055","#d63031","#e84393","#2d3436"
+        ],
       currentIndex: null
     };
   },
   created() {},
   methods: {
+    a(index){
+      this.currentIndex = index
+    },
+    b(){
+      this.currentIndex = null
+    },
     copy(index) {
       //点击音效
       var audio = document.querySelector("#audio");
@@ -64,8 +123,13 @@ export default {
 <style scoped>
 .line1 {
   display: flex;
-  height: 25vh;
+  height: 100vh;
   width: 100vw;
+  /* height: 800px;
+  width: 800px; */
+
+  flex-wrap: wrap;    /* 使其多行进行flex布局 */
+  align-content: flex-start; /* 交叉轴上从start开始排列 */
 }
 /* 所有盒子通用样式 */
 .box {
@@ -76,28 +140,86 @@ export default {
   line-height: 24vh;
 
   /* 小盒子大小 */
-  height: 24vh;
-  width: 25vw;
+  height: 25%;
+  width: 20%;
 
   /* 盒子的圆角 */
-  border-radius: 8px;
+  /* border-radius: 8px; */
 
   /* 行内块级元素 好让各个盒子在同一行 */
   display: inline-block;
-  flex: 1;
+  /* flex: 1; */
 
   /* margin: 10px; */
 }
 
 /* 各自小盒子相关 */
-.black {
-  background: black;
-}
-.orange {
+.a {
   background: #55efc4;
 }
+.b {
+  background: #81ecec;
+}
+.c {
+  background: #74b9ff;
+}
+.d {
+  background: #a29bfe;
+}
+.e {
+  background: #dfe6e9;
+}
+/* -------------------5------------------------ */
+.f{
+  background: #00b894;
+}
+.g{
+  background: #00cec9;
+}
+.h{
+  background: #0984e3;
+}
+.i{
+  background: #6c5ce7;
+}
+.j{
+  background: #b2bec3;
+}
+/* -------------------10------------------------ */
+.k{
+  background: #ffeaa7;
+}
+.l{
+  background: #fab1a0;
+}
+.m{
+  background: #ff7675;
+}
+.n{
+  background: #fd79a8;
+}
+.o{
+  background: #636e72;
+}
+/* -------------------15------------------------ */
+.p{
+  background: #fdcb6e;
+}
+.q{
+  background: #e17055;
+}
+.r{
+  background: #d63031;
+}
+.s{
+  background: #e84393;
+}
+.t{
+  background: #2d3436;
+}
+
 .box:hover {
-  transform: scale(0.88);
+  transform: scale(0.95);
 }
 .copy {
   height: 50px;
